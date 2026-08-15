eb5dbb1462de8f70609ba274609a048b9633bfa6
Gregor Samsa <fgiannis171@gmail.com>
2026-08-15 13:49:44 +0300

    Completed generator engines mvc

--- src/bog_simulator/core/consumers/dfde.py
+++ src/bog_simulator/core/consumers/dfde.py
@@ -2,16 +2,35 @@ from bog_simulator.physics.thermodynamics import dfde_consumption
 from bog_simulator.physics import per
 
 class DFDE:
-    def __init__(self, type, output):
-        self.type = type
-        self.update(output)
+    output_limits = {
+        "W8L34DF": {"min": 1400, "max": 4000},
+        "W6L34DF": {"min": 1000, "max": 3000}
+    }
+
+    def __init__(self, **genset_params):
+        config = genset_params or {}
+
+        self.type = config["type"]
+
+        self.max_output = self.output_limits[self.type]["max"]
+        self.min_output = self.output_limits[self.type]["min"]
+
+        self._is_operating = config["is_operating"]
+        self.update(config["output"])
 
     def update(self, output):
-        self.output = output
-        m_gas_new = dfde_consumption(self.type, output)
-        self.m_gas = per.sec(m_gas_new)
+        if  self.min_output <= output <= self.max_output: 
+            self.output = output
+            m_gas_new = self._is_operating * dfde_consumption(self.type, output)
+            self.m_gas = per.sec(m_gas_new)
+
 
 
 if __name__ == '__main__':
-    dfde = DFDE("W8L34DF", 2600)
+    generator_parameters = {
+        "type": 'W8L34DF',
+        "is_operating": False,
+        "output": 3500
+    }
+    dfde = DFDE(**generator_parameters)
     print(per.hour(dfde.m_gas))
---
+++ src/bog_simulator/ui/consumers_ui/dfde_ui.py
@@ -0,0 +1,87 @@
+from tkinter import ttk
+import tkinter as tk
+
+from bog_simulator.core.consumers import dfde
+
+from bog_simulator.utils import resize_img as resize
+
+from bog_simulator.ui.components import LabelInput, LabelValue
+
+from bog_simulator.physics import per
+
+from bog_simulator.config import RESOURCE_DIR
+
+image = RESOURCE_DIR / "dfde_img.png"
+
+class DFDE_UI(tk.Frame):
+    def __init__(self, parent, model: dfde.DFDE, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+
+        self.model = model
+
+        self.output = tk.IntVar(value= self.model.output)
+        self.m_gas = tk.DoubleVar(value= round(per.hour(self.model.m_gas), 2))
+        
+        self._img = resize.import_image(image, width= 50, height= 70)
+        self._draw_hmi()
+
+    def _draw_hmi(self):
+        #set image of the consumer
+        self.canvas = tk.Canvas(self, width= 50, height= 70)
+        self.canvas.create_image(25, 35, image= self._img)
+
+        #output controller
+        input_args = {
+            'from_': self.model.min_output,
+            'to': self.model.max_output,
+            'increment': 50,
+            'textvariable': self.output,
+            'width': 10
+        }
+        label_args = {'text': "Kw"}
+        self.output_ui = LabelInput(self, ttk.Spinbox, input_args=input_args, label_args=label_args)
+
+        #gas consumption value
+        self.m_gas_ui = LabelValue(self, self.m_gas, "Consumption")
+
+        #type of genset
+        self.type_label = tk.Label(self, text=self.model.type)
+
+        self.output_ui.pack()
+        self.canvas.pack()
+        self.type_label.pack()
+        self.m_gas_ui.pack()
+
+    def update(self):
+        #receive value from controller
+        output_new = self.output.get()
+
+        #update model
+        self.model.update(output_new)
+
+        #print current gas consumption to the user
+        self.m_gas.set(round(per.hour(self.model.m_gas), 2))
+
+        
+
+if __name__ == "__main__":
+    generator1_parameters = {
+            "type": 'W8L34DF',
+            "is_operating": True,
+            "output": 3500
+        }
+    generator2_parameters = {
+                "type": 'W6L34DF',
+                "is_operating": True,
+                "output": 2650
+            }
+    root = tk.Tk()
+
+    dfde_ui1 = DFDE_UI(root, dfde.DFDE(**generator1_parameters))
+    defde_ui2 = DFDE_UI(root, dfde.DFDE(**generator2_parameters))
+    dfde_ui1.grid(row=0, column=0)
+    defde_ui2.grid(row=0, column= 1)
+
+    root.mainloop()
+
+
